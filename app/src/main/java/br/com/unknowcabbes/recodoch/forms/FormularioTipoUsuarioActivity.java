e35379ec65519c49d4bd07c2c5b9c7427c2c3d40
Refluenchai61 <victorqs1997@hotmail.com>
2018-09-11 02:10:16 -0300

    Criação formulario Tipo usuario

---
+++ app/src/main/java/br/com/unknowcabbes/recodoch/forms/FormularioTipoUsuarioActivity.java
@@ -0,0 +1,14 @@
+package br.com.unknowcabbes.recodoch.forms;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import br.com.unknowcabbes.recodoch.R;
+
+public class FormularioTipoUsuarioActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_formulario_tipo_usuario);
+    }
+}
